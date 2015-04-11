f4bdc6f175d805db7c4eae99706de260c90a2ccd
Frazer McIntosh <me@frazerm.com>
2015-04-12 00:35:40 +0100

    pointer added

--- client/js/leapmotion.js
+++ client/js/leapmotion.js
@@ -1,57 +1,3 @@
-// var toType = function(obj) {
-//     return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
-// }
-
-// var SENSITIVITY = 10
-
-// var touchLast = []
-
-// Leap.loop(function (frame) {
-//     if(frame.valid) {
-//         handOne = frame.hands[0]
-//         handTwo = frame.hands[1]
-
-//         if(handTwo) {
-//             //two hands, do a zoom
-//             touchLast = [];
-//             indexPosOne = handOne.indexFinger.tipPosition;
-//             indexPosTwo = handTwo.indexFinger.tipPosition;
-
-//             console.log("Two fingers");
-//         } else if(handOne && handOne.indexFinger.tipPosition[2] < 0) {
-//             indexPos = handOne.indexFinger.tipPosition;
-//             //check for pan
-//             if(touchLast.length > 0) {
-//                 delta = [indexPos[0] - touchLast[0], indexPos[1] - touchLast[1]];
-//                 if(Math.abs(delta[0]) < SENSITIVITY) {
-//                     delta[0] = 0;
-//                 }
-//                 if(Math.abs(delta[1]) < SENSITIVITY) {
-//                     delta[1] = 0;
-//                 }
-//                 if(delta[0] || delta[1]) {
-//                     Weather.mapWrapper.panByDelta(delta);
-//                     //pan(delta[0], delta[1])
-//                 }
-//             };
-//             touchLast = [indexPos[0], indexPos[1]];
-//             console.log("One finger press");
-//         } else if(frame.gestures.length > 0) {
-//             touchLast = [];
-//             frame.gestures.forEach(function(gesture) {
-//                 if(gesture.center) {
-//                     //got a circlegesture
-
-//                     var normal = gesture.normal;
-//                     normal[0] < 0 ? Weather.mapWrapper.zoomIn() : Weather.mapWrapper.zoomOut();
-//                 }
-//             });
-//         } else {
-//             touchLast = [];
-//         };
-//     }
-
-// });
         var spinClockwise = function () {
             console.log("spin clockwise")
         }
@@ -75,11 +21,37 @@
         var touchLast = []
         var grabLast = 0
 
+        var pointers = []
+
+        var Pointer = function () {
+            var pointer = this;
+            var img = document.createElement('img');
+            img.src = 'circle_red.png';
+            img.style.position = 'absolute';
+            img.onload = function () {
+                pointer.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
+            document.body.appendChild(img);
+  }
+
+            pointer.setTransform = function (pos) {
+                img.style.left = pos[0] - img.width  / 2 + 'px';
+                img.style.top  = pos[1] - img.height / 2 + 'px';
+            }
+        }
+
+        pointers[0] = new Pointer();
+
         Leap.loop(function (frame) {
             if(frame.valid) {
                 handOne = frame.hands[0]
                 handTwo = frame.hands[1]
-            
+
+                if(handOne) {
+                    var pointerOne = ( pointers[0] || (pointers[0] = new Pointer()) );
+
+                    pointerOne.setTransform(handOne.screenPosition());
+                }
+
                 if(handTwo) {
                     //two hands, do a zoom
                     touchLast = [];
@@ -129,4 +101,4 @@
                 };
             }
 
-        });
+        }).use('screenPosition', {scale: 0.5});
